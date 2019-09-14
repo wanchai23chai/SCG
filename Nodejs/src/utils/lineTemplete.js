0131711e9d677c8167e4268d7999a310e020fa93
wanchai nupinit <wanchai@magicboxasia.com>
2019-09-15 02:16:07 +0700

    chage response line msg

--- Nodejs/src/utils/lineTemplete.js
+++ Nodejs/src/utils/lineTemplete.js
@@ -1,85 +1,74 @@
-export function displayMaxMin(meta){
+export function displayMaxMin(meta) {
   let bubble = {
-    "type": "carousel",
-    "contents": [
-      {
-        "type": "bubble",
-        "size": "mega",
-        "header": {
+    "type": "flex",
+    "altText": "this is a flex message",
+    "contents": {
+      "type": "bubble",
+      "header": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [{
+          "type": "text",
+          "text": `ข้อมูลสรุปของหุ้น ${meta.ric}`,
+          "color": "#ffffff",
+          "align": "start",
+          "size": "xl",
+          "gravity": "center"
+        }, {
+          "type": "text",
+          "text": `จากวันที่ ${meta.startDate} ถึง ${meta.endDate}`,
+          "color": "#ffffff",
+          "align": "start",
+          "size": "xs",
+          "gravity": "center"
+        }],
+        "backgroundColor": "#27ACB2",
+        "paddingTop": "19px",
+        "paddingAll": "12px",
+        "paddingBottom": "16px"
+      },
+      "body": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [{
           "type": "box",
-          "layout": "vertical",
-          "contents": [
-            {
-              "type": "text",
-              "text": `Summary of ${meta.ric}`,
-              "color": "#ffffff",
-              "align": "start",
-              "size": "xl",
-              "gravity": "center"
-            }, {
-              "type": "text",
-              "text": `from ${meta.startDate} to ${meta.endDate}`,
-              "color": "#ffffff",
-              "align": "start",
-              "size": "xs",
-              "gravity": "center"
-            }
-          ],
-          "backgroundColor": "#27ACB2",
-          "paddingTop": "19px",
-          "paddingAll": "12px",
-          "paddingBottom": "16px"
-        },
-        "body": {
-          "type": "box",
-          "layout": "vertical",
-          "contents": [
-            {
-              "type": "box",
-              "layout": "horizontal",
-              "spacing": "md",
-              "contents": [
-                {
-                  "type": "text",
-                  "text": `Maximum High : ${Math.max(...meta.sum.max).toString()} `,
-                  "color": "#8C8C8C",
-                  "size": "sm",
-                  "wrap": true
-                },
-                {
-                  "type": "separator"
-                },
-                {
-                  "type": "text",
-                  "text":  `Minimum Low : ${Math.min(...meta.sum.min).toString()}`,
-                  "color": "#8C8C8C",
-                  "size": "sm",
-                  "wrap": true
-                }
-              ],
-              "flex": 1
-            }
-          ],
+          "layout": "horizontal",
           "spacing": "md",
-          "paddingAll": "12px"
-        },
-        "footer": {
-          "type": "box",
-          "layout": "vertical",
-          "contents": [
+          "contents": [{
+              "type": "text",
+              "text": `ค่าสูงสุดของ High : ${Math.max(...meta.sum.max).toString()} `,
+              "color": "#8C8C8C",
+              "wrap": true
+            },
             {
-              "type": "button",
-              "style": "secondary",
-              "action": {
-                "type": "uri",
-                "label": "Go to details",
-                "uri": `${meta.url}`
-              }
+              "type": "separator"
+            },
+            {
+              "type": "text",
+              "text": `ค่าต่ำสุดของ Low : ${Math.min(...meta.sum.min).toString()}`,
+              "color": "#8C8C8C",
+              "wrap": true
             }
-          ]
-        }
+          ],
+          "flex": 1
+        }],
+        "spacing": "md",
+        "paddingAll": "12px"
+      },
+      "footer": {
+        "type": "box",
+        "layout": "horizontal",
+        "contents": [{
+          "type": "button",
+          "style": "secondary",
+          "action": {
+            "type": "uri",
+            "label": "Go to details",
+            "uri": `${meta.url}`
+          }
+        }]
       }
-    ]
+    }
   }
-return bubble
+  return bubble
 }
